a951bd7b161bf0e7a4adc8931dc6854bb7b4f6b5
sandobits <sandbar.dev@gmail.com>
2020-12-21 10:03:01 -0200

    Use subject on class #12

--- calculator/lib/calculator.rb
+++ calculator/lib/calculator.rb
@@ -1,5 +1,5 @@
 class Calculator
-  def self.sum(a, b)
+  def sum(a, b)
     return (a+b).round(2)
   end
 end
--- calculator/spec/calculator/calculator_spec.rb
+++ calculator/spec/calculator/calculator_spec.rb
@@ -1,26 +1,26 @@
 require 'calculator'
 
-describe Calculator do
-
-  context ".sum" do
+describe 'Classe Calculadora' do
+  subject() { Calculator.new() }
+  context "#sum" do
     it "with positive numbers" do
-      result = Calculator.sum(5, 6) # interaction
+      result = subject.sum(5, 6) # interaction
       expect(result).to eql(11) # verification
       # teardown???
     end
 
     it "with positive and negative numbers" do
-      result = Calculator.sum(5, -6)
+      result = subject.sum(5, -6)
       expect(result).to eql(-1)    
     end
 
     it "with negative numbers" do
-      result = Calculator.sum(-5, -6)
+      result = subject.sum(-5, -6)
       expect(result).to eql(-11)    
     end
 
     it "with floating point numbers" do
-      result = Calculator.sum(5.2, 6.4)
+      result = subject.sum(5.2, 6.4)
       expect(result).to eql(11.6)
     end
   end
