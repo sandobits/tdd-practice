f685ed10d7ccc4ad835a53eace74cbff35acca3f
Sanderson <sanderson.depaula@m4u.com.br>
2021-03-02 16:48:58 -0300

    add array & range matchers

--- spec/matches/arrays/array_spec.rb
+++ spec/matches/arrays/array_spec.rb
@@ -1,2 +1,18 @@
-describe 'Matchers de Arrays' do
+# frozen_string_literal: true
+
+describe [1, 2, 3], 'Matchers de Arrays' do
+  it '#include' do
+    expect(subject).to include(2)
+    expect(subject).to include(2, 1)
+  end
+
+  it '#contain_exactly' do
+    # ordenacao nao importa
+    expect(subject).to contain_exactly(1, 3, 2)
+  end
+
+  it '#match_array' do
+    # ordenacao nao importa, mas o argumento eh um array
+    expect(subject).to match_array([1, 3, 2])
+  end
 end
---
+++ spec/matches/ranges/ranges_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+describe (1..10), 'Ranges' do
+  it '#cover' do
+    # basically 'include'
+    expect(subject).to cover(2)
+    expect(subject).to cover(2, 5)
+    expect(subject).not_to cover(0, 6)
+  end
+end
